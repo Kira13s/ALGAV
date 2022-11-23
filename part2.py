28a5a628058348d2b9af8434e7342ac5f18b74c9
28600518 <lauryn.pierre@etu.sorbonne-universite.fr>
2022-11-23 11:02:31 +0100

    fin partie 2

--- part2.py
+++ part2.py
@@ -1,5 +1,6 @@
 from start import table
 from math import log2
+import os
 
 class BDD:
     counter = 0
@@ -32,30 +33,24 @@ def luka (T:BDD)->str:
         T.luka=f"{T.val}({luka(T.left)})({luka(T.right)})"
         return T.luka
 
+def suffix(n, D):
+    if n.left and n.right:
+        if n.left.luka not in D:
+            D = suffix(n.left, D)
+        if n.right.luka not in D:
+            D = suffix(n.right, D)
+        if n.luka not in D:
+            D[n.luka] = BDD(n.val, D[n.left.luka], D[n.right.luka])
+    return D
+        
+
 def compression_luka(T:BDD): # TO DOO
     if T is None:
         return None
-    file = [T]
-    D = dict()
-
-    while len(file) > 0:
-        node = file.pop(0)
-        if node.left and node.right:
-            luka_l = luka(node.left)
-            if luka_l in D:
-                node.left = D[luka_l]
-            else:
-                D[luka_l] = node.left
-                file.append(node.left)
-            luka_r = luka(node.right)
-            if luka_r in D:
-                node.right = D[luka_r]
-            else:
-                D[luka_r] = node.right
-                file.append(node.right)
-        else:
-            break
-    return T
+    D = {"True": BDD(True), "False": BDD(False)}
+    luka_racine = luka(T)
+    D = suffix(T, D)
+    return D[luka_racine]
 
 
 def compression(T:BDD,d:dict={},i=-1):  # TO DOO
@@ -78,29 +73,33 @@ def compression(T:BDD,d:dict={},i=-1):  # TO DOO
     print(d)
     pass
 
-def traversal(root, pos):
+def traversal(n, D):
     res = ""
-    if root:
-        res += str(pos) + "[ label =\"" + str(root.val) + "\"];\n"
-        if root.left:
-            res += str(pos) + "--" + str(pos + 1) + "[style=dashed]\n"
-        temp = pos
-        new_res, pos = traversal(root.left,pos+1)
-        res = res + new_res
-        if root.right:
-            res += str(temp) + "--" + str(pos) + "\n"
-        new_res, pos = traversal(root.right,pos)
-        res = res + new_res
-    return res,pos
+    if n:
+        if n not in D:
+            pos = len(D)
+            res += str(pos) + " [ label = \"" + str(n.val) + "\" ];\n"
+            D[n] = str(pos)
+            if n.left and n.right:
+                new_res, D = traversal(n.left, D)
+                res += new_res + D[n] + "--" + D[n.left] + " [style=dashed];\n" 
+                new_res, D = traversal(n.right, D)
+                res += new_res + D[n] + "--" + D[n.right] + ';\n'
+
+    return res, D
 
 def dot (T:BDD, filename: str):  # TO DOO
     file = open(filename + ".dot", "w")
     s = "graph { \n"
-    res,_ = traversal(T, 1)
+    res,_ = traversal(T, dict())
     s += res + "}"
     file.write(s)
     file.close()
     
+#générer un fichier pdf du graphe de l'arbre
+def show(T:BDD, filename: str):
+    dot(T,filename)
+    os.system("dot -Tpdf "+filename + ".dot -o " + filename + ".pdf")
 
     
 if __name__ == "__main__" :
@@ -112,5 +111,6 @@ if __name__ == "__main__" :
     #t=luka(abr)
     #print(abr)
     abr_com = compression_luka(abr)
-    #dot(abr,"test")
+    show(abr_com, "test_com")
+    show(abr,"test")
     
